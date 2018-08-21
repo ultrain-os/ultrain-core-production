82ebcfbf88623d1c6d5e38a6810cfa3af2628939
Yufeng Shen <syf1984@gmail.com>
2018-08-21 17:35:55 +0800

    fix contract block_timestamp's block_interval_ms

--- contracts/ultrainiolib/time.hpp
+++ contracts/ultrainiolib/time.hpp
@@ -156,7 +156,7 @@ namespace ultrainio {
          bool   operator ==( const block_timestamp& t )const   { return slot == t.slot; }
          bool   operator !=( const block_timestamp& t )const   { return slot != t.slot; }
          uint32_t slot;
-         static constexpr int32_t block_interval_ms = 500;
+         static constexpr int32_t block_interval_ms = 10 * 1000;
          static constexpr int64_t block_timestamp_epoch = 946684800000ll;  // epoch is year 2000
 
          ULTRAINLIB_SERIALIZE( block_timestamp, (slot) )
