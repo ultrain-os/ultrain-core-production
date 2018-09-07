e1978334cf096f9cc452519df00195c22ae3f0f2
Yufeng Shen <syf1984@gmail.com>
2018-09-07 11:54:49 +0800

    expire early

--- consensus/rpos/src/Node.cpp
+++ consensus/rpos/src/Node.cpp
@@ -22,7 +22,7 @@ using namespace boost::asio;
 using namespace std;
 
 namespace ultrainio {
-    char version[]="f00170";
+    char version[]="c218cc";
 
     const int UranusNode::MAX_ROUND_SECONDS = 10;
     const int UranusNode::MAX_PHASE_SECONDS = 5;
