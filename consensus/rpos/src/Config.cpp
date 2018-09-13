e86eab9197135342d7a2bd3f750804ffb906127d
ym <mathshero@163.com>
2018-09-13 17:18:22 +0800

    change proposer voter from 20.0 to 10.0

--- consensus/rpos/src/Config.cpp
+++ consensus/rpos/src/Config.cpp
@@ -4,7 +4,7 @@ namespace ultrainio {
     const int Config::kAverageBlockPerMinutes = 6;
 
     const double Config::VOTER_STAKES_NUMBER = 1000.0;
-    const double Config::PROPOSER_STAKES_NUMBER = 20.0;
+    const double Config::PROPOSER_STAKES_NUMBER = 10.0;
     const int Config::DEFAULT_THRESHOLD = 6000;
     const int Config::MAX_LATER_NUMBER = 3;
 }
