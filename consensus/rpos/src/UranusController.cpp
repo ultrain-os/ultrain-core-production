3725ce805a8b1b3c259fd4203d990ee3620bd12b
Honglu Lei <leihonglu@ultrain.io>
2018-09-25 11:48:41 +0800

    revert the code during debugging the env issue

--- consensus/rpos/src/UranusController.cpp
+++ consensus/rpos/src/UranusController.cpp
@@ -1356,10 +1356,10 @@ namespace ultrainio {
         const auto& cfg = chain.get_global_properties().configuration;
 
         //add optimization for not to run trxs multiple times in same block
-        if (m_ba0BlockVerified) {
+        /*if (m_ba0BlockVerified) {
            m_voterPreRunBa0InProgress = true;
            return true;
-        }
+        }*/
 
         chain.abort_block();
         const chain::signed_block &block = m_ba0Block;
