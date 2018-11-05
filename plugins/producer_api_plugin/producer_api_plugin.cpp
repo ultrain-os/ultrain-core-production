dc6606dc510323b35e8ecefba2561a87e227a64a
刘伟 <liuwei@ultrain.io>
2018-11-05 14:59:55 +0800

    feature worldstate api interface

--- plugins/producer_api_plugin/producer_api_plugin.cpp
+++ plugins/producer_api_plugin/producer_api_plugin.cpp
@@ -82,6 +82,8 @@ void producer_api_plugin::plugin_startup() {
        //     INVOKE_V_R(producer, remove_greylist_accounts, producer_plugin::greylist_params), 201), 
        //CALL(producer, producer, get_greylist,
        //     INVOKE_R_V(producer, get_greylist), 201),                 
+       CALL(producer, producer, create_snapshot,
+            INVOKE_R_V(producer, create_snapshot), 201),
    });
 }
 
