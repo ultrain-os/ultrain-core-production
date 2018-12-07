1308f76da70f54ee3b1829d8b6c8b7958844b82a
ym <ym@ymdeMacBook-Pro.local>
2018-12-07 11:49:47 +0800

    Add test scripts for event notification.

---
+++ tests/listen_event.js
@@ -0,0 +1,36 @@
+http = require('http');
+fs = require('fs');
+server = http.createServer( function(req, res) {
+
+    console.dir(req.param);
+
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+            console.log("Body: " + body);
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+    }
+    else
+    {
+        console.log("GET");
+       
+        var html = fs.readFileSync('index.html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+    }
+
+});
+
+port = 3000;
+host = '127.0.0.1';
+server.timeout = 0;
+server.keepAliveTimeout = 0;
+server.listen(port, host);
+console.log('Listening at http://' + host + ':' + port);
---
+++ tests/unreg_event_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import json
+import requests
+
+url = "http://127.0.0.1:8888/v1/chain/unregister_event"
+#content = [{"ref_block_num":21453,"ref_block_prefix":3165644999,"expiration":"2017-12-08T10:28:49","scope":["initb","initc"],"read_scope":[],"messages":[{"code":"currency","type":"transfer","authorization":[{"account":"initb","permission":"active"}],"data":"000000008093dd74000000000094dd74e803000000000000"}],"signatures":[]}, ["UTR6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV"], ""]
+
+content = {"account":"hello","post_url":"http://127.0.0.1:3000/v1/listen_event"}
+json_content = json.dumps(content)
+print json_content
+r=requests.post(url,data=json_content)
+print r.text
