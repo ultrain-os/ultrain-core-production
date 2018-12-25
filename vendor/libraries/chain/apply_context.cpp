6a98c05e40b9c4b94fa941f5fe64a64a4366f94d
fanliangqin <fanliangqin@ultrain.io>
2018-12-25 15:38:05 +0800

    fix bug: db_drop_i64 did not remove key_value_object

--- vendor/libraries/chain/apply_context.cpp
+++ vendor/libraries/chain/apply_context.cpp
@@ -639,11 +639,12 @@ int apply_context::db_drop_i64(uint64_t code, uint64_t scope, uint64_t table) {
 
    int64_t usage_delta = 0LL;
    std::for_each(lower, upper, [&](const key_value_object& obj) {
-      usage_delta += (obj.value.size() + config::billable_size_v<key_value_object>);
+      usage_delta = (obj.value.size() + config::billable_size_v<key_value_object>);
+      update_db_usage( obj.payer,  -(usage_delta) );
+      db.remove(obj);
    });
 
    keyval_cache.purge_table_cache(table_obj->id);
-   update_db_usage( table_obj->payer,  -(usage_delta) );
    remove_table(*table_obj);
 
    return 0;
