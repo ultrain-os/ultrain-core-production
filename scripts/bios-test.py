82b983c1e834f9f569645b0d1b774245374e87d6
yanhuichao <739884701@qq.com>
2019-01-08 11:37:13 +0800

    bios initaccount resourcelease

--- scripts/bios-test.py
+++ scripts/bios-test.py
@@ -619,6 +619,7 @@ def stepCreateinitAccounts():
     sleep(10)
     for a in initialAccounts:
         retry(args.clultrain + 'transfer  ultrainio  %s  "%s UGAS" '  % (a,"100000000.0000"))
+        retry(args.clultrain + 'system resourcelease ultrainio  %s  10 100' % a)
     retry(args.clultrain + 'system resourcelease ultrainio  hello  6000 10')
     retry(args.clultrain + 'transfer ultrainio utrio.rand "10000 UGAS" ')
     retry(args.clultrain + 'set account permission utrio.rand active \'{"threshold":1,"keys": [{"key": "%s","weight": 1}],"accounts": [{"permission":{"actor":"utrio.rand","permission":"utrio.code"},"weight":1}]}\' owner -p utrio.rand' % (args.public_key))
