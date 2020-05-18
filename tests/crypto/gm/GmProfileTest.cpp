6cd0cc48a203e19185cc9a8a51b2d9932f7b9f8d
xiaofen <xiaofen.qin@gmail.com>
2020-05-18 17:15:21 +0800

    refactor ed25519/sm2/secp256k1 performance test

--- tests/crypto/gm/GmProfileTest.cpp
+++ tests/crypto/gm/GmProfileTest.cpp
@@ -12,7 +12,10 @@
 #include "gm/sm2/PublicKey.h"
 #include "gm/sm2/Signature.h"
 
+#include <ed25519/Ed25519.h>
+
 using namespace gm;
+using namespace ed25519;
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -25,6 +28,7 @@ int main(int argc, char* argv[]) {
 
     fc::sha256 data = fc::sha256::hash(std::string((char*)msg));
 
+    // sm2
     std::vector<gm::sm2::PrivateKey> gmPriV;
     std::vector<gm::sm2::PublicKey> gmPubV;
     std::vector<gm::sm2::Signature> gmSigV;
@@ -51,15 +55,41 @@ int main(int argc, char* argv[]) {
         ecp256SigV.push_back(pri.sign(data));
     }
 
-    std::chrono::steady_clock::time_point pointStart1 = std::chrono::steady_clock::now();
+    // ed25519
+    std::vector<uint8_t*> ed25519PriKeyV;
+    std::vector<uint8_t*> ed25519PubKeyV;
+    std::vector<uint8_t*> ed25519SigV;
+    for (int i = 0; i < n; i++) {
+        uint8_t* priKey = (uint8_t*)malloc(Ed25519::PRIVATE_KEY_LEN);
+        ed25519PriKeyV.push_back(priKey);
+        uint8_t* pubKey = (uint8_t*)malloc(Ed25519::PUBLIC_KEY_LEN);
+        ed25519PubKeyV.push_back(pubKey);
+        uint8_t* sig = (uint8_t*)malloc(Ed25519::SIGNATURE_LEN);
+        ed25519SigV.push_back(sig);
+        Ed25519::keypair(ed25519PubKeyV[i], ed25519PriKeyV[i]);
+        Ed25519::sign(ed25519SigV[i], msg, strlen((char*)msg), ed25519PriKeyV[i]);
+    }
+
+    std::chrono::steady_clock::time_point ed25519PointStart = std::chrono::steady_clock::now();
+    for (int i = 0; i < n; i++) {
+        bool ok = Ed25519::verify(msg, strlen((char*)msg), ed25519SigV[i], ed25519PubKeyV[i]);
+        if (!ok) {
+            std::cout << "ed25519 verify failed." << std::endl;
+        }
+    }
+    std::chrono::microseconds ed25519Total = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()
+            - ed25519PointStart);
+
+    std::chrono::steady_clock::time_point pointStartSecp256K1 = std::chrono::steady_clock::now();
     for (int i = 0; i < n; i++) {
         if (std::string(ecp256PubV[i]) != std::string(fc::crypto::public_key(ecp256SigV[i], data))) {
             std::cout << "secp256k1 verify error" << std::endl;
         }
     }
-    std::chrono::microseconds total1 = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - pointStart1);
+    std::chrono::microseconds secp256K1Total = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()
+            - pointStartSecp256K1);
 
-    std::chrono::steady_clock::time_point pointStart = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point sm2PointStart = std::chrono::steady_clock::now();
     for (int i = 0; i < n; i++) {
         gm::sm2::PublicKey pub = gmPubV[i];
         bool ok = pub.verify(data.data(), data.data_size(), gmSigV[i]);
@@ -67,8 +97,16 @@ int main(int argc, char* argv[]) {
             std::cout << "gm verify error. at " << i << std::endl;
         }
     }
-    std::chrono::microseconds total = std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::steady_clock::now() - pointStart);
-    std::cout << "ecdsa verify one time:" << total1.count() / n << "; gm verify one time: " << total.count() / n << " microseconds " << std::endl;
+    std::chrono::microseconds sm2Total = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::steady_clock::now() - sm2PointStart);
+
+    for (int i = 0; i < n; i++) {
+        free(ed25519PriKeyV[i]);
+        free(ed25519PubKeyV[i]);
+        free(ed25519SigV[i]);
+    }
+    std::cout << "ed25519 verify one time:" << ed25519Total.count() / n << std::endl;
+    std::cout << "sm2 verify one time: " << sm2Total.count() / n << " microseconds " << std::endl;
+    std::cout << "secp256k1 verify one time:" << secp256K1Total.count() / n << std::endl;
     return 0;
 }
